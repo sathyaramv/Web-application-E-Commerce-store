154688906be2a941685934e95ecc9112c5db5d67
Sathya <32465233+sathyaramv@users.noreply.github.com>
2019-10-17 12:57:42 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
-var path = require('path');
 var express = require('express');
 var app = express();
-app.set('view engine', 'ejs');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+const path = require('path');
+
+var router = express.Router();
+
+var catalogController = require('./controller/catalogController.js');
+
 app.set('views', path.join(__dirname, 'views'));
+app.set('view engine','ejs');
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({secret: "myCollection"}));
 
+app.use('/',catalogController);
+app.use('/catalog',catalogController);
+app.use('/about',catalogController);
+app.use('/contactUs',catalogController);
+app.use('/catalog/item/:id',catalogController);
+app.use('/*',catalogController);
 
-var home = require('./routes/controller');
-app.use('/',home);
-module.exports = app;
-app.listen(8080);
-console.log('listening to port 8080');
+app.listen('8080',function(req,res){
+  console.log("Server started..listening to port 8080");
+});
